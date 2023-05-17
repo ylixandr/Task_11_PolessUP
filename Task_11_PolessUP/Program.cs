eb03412b7086e6203ce3824eb544af75c83ab813
Ylixandr <savoshinsky@inbox.ru>
2023-05-17 15:29:29 +0300

    Добавьте файлы проекта.

---
+++ Task_11_PolessUP/Program.cs
@@ -0,0 +1,24 @@
+﻿    static void SortAscending(int[] array)
+    {
+        
+
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            for (int j = 0; j < array.Length - 1 - i; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+int[] array = new int[] { 2, 4, 6, 1, 88, 19 };
+SortAscending(array);
+for (int i = 0; i<array.Length;i++)
+{
+    Console.WriteLine(array[i]);
+}
+
